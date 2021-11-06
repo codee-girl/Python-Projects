0f71e6e45ca03b29d91b03263df128feb6e6f898
Rabs <karkirabu@gmail.com>
2021-11-06 23:34:51 +0545

    prettytable

---
+++ practice/prettytable.py
@@ -0,0 +1,13 @@
+from prettytable import PrettyTable
+
+table=PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirrle","Combee"])
+table.add_column("Type",["Fire","Water","Earth"])
+
+table.align
+
+print(table)
+
+
+
+
