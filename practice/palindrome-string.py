4542a5cb6cbf18967c86c57fcf25087a113615ab
Rabs <karkirabu@gmail.com>
2021-11-10 23:32:26 +0545

    palindrome-programs

---
+++ practice/palindrome-string.py
@@ -0,0 +1,17 @@
+#using reversed function 
+str=input("Enter a String : ")
+str.casefold()
+rev= reversed(str)
+
+if list(str)==list(rev):
+    print("It is palindrome")
+else:
+    print("Not a palindrome")    
+
+#usine [::-1]):  
+
+string=input(("Enter a string:"))  
+if(string==string[::-1]):  
+      print("The letter is a palindrome")  
+else:  
+      print("The letter is not a palindrome")  
